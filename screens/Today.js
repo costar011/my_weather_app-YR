65276a98e0d19a5413f333750b0a6e4089e175d0
costar011 <dnaer456@gmail.com>
2021-04-04 20:49:24 +0900

    code err

--- screens/Today.js
+++ screens/Today.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Image } from "react-native";
+import * as Location from "expo-location";
+import { getDate } from "../src/commonUtil";
+import { LinearGradient } from "expo-linear-gradient";
+import TypeWriter from "react-native-typewriter";
 
 const Today = () => {
   return (
-    <View>
-      <View>
-        <Text>sss</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>test</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    alignItems: `center`,
+    justifyContent: `center`,
+  },
+});
 
 export default Today;
