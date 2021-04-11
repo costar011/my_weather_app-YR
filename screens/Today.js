1606cd4daf597109d387c918f5dde5ca95b07413
costar011 <dnaer456@gmail.com>
2021-04-11 21:46:34 +0900

    err

--- screens/Today.js
+++ screens/Today.js
@@ -1,25 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
 import * as Location from "expo-location";
-import { getDate } from "../src/commonUtil";
+import { getCurrentDate } from "../src/commonUtil";
 import { LinearGradient } from "expo-linear-gradient";
 import TypeWriter from "react-native-typewriter";
 
-const Today = () => {
+const WEATHER_API_KEY = "c2279690f1a92e1324cfa1a79d5584ed";
+
+const TodayScreen = () => {
+  const [location_S, setLocation_S] = useState(null);
+  const [errMsg_S, setErrMsg_S] = useState(``);
+
+  const [viewDate, setViewDate] = useState(`0000. 00. 00 (0)`);
+  const [viewTime, setviewTime] = useState(`00:00`);
+
+  const [currentTemp, setCurrentTemp] = useState(`0`);
+  const [currentCity, setCurrentCity] = useState(``);
+
+  const [minTemp, setMinTemp] = useState(``);
+  const [maxTemp, setMaxTemp] = useState(``);
+
+  const [weatherStatus, setWeatherStatus] = useState(``);
+  const [weatherImg, setWeatherImg] = useState(null);
+
+  setInterval(() => {
+    const { currentDate, currentTime } = getCurrentDate();
+
+    setViewDate(currentDate);
+    setviewTime(currentTime);
+  }, 1000);
+
   return (
     <View style={styles.container}>
-      <Text>test</Text>
+      <Text>error</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 2,
 
     alignItems: `center`,
     justifyContent: `center`,
   },
 });
 
-export default Today;
+export default TodayScreen;
